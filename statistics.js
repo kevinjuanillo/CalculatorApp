f8e0571458a29cc871f0f588838525de45100aa6
Kevin.Juanillo <Kevin.Juanillo@vertiv.com>
2024-05-21 22:29:08 +0800

    [KJ]: useful comments

--- statistics.js
+++ statistics.js
@@ -7,26 +7,26 @@ function inputValidation(operation) {
 
     // iterate over each item in the array
     for (let i = 0; i < arrayNums.length; i++ ) {
-        if (checkInputNumberOnly(arrayNums[i])) {
+        if (checkInputNumberOnly(arrayNums[i])) { //check one by one if input is number only
             isValid = true;
         }
         else {
             isValid = false;
-        alert("input invalid"); 
+        alert("input invalid"); // set validation message
         break;
         }
     }
 
     if (isValid) {
-        switch (operation) {
+        switch (operation) { // case operator for the 3 functions of the calculator
             case 1:
-                getMean(1);
+                getMean(1); // call getmean function pass parameter 1
                 break;
             case 2:
-                getVariance(1);
+                getVariance(1); // call getVariance function pass parameter 1
                 break;
             case 3:
-                getStandardDeviation(1);
+                getStandardDeviation(1); // call getStandardDeviation function pass parameter 1
                 break;
         }
     }
@@ -38,11 +38,11 @@ function getStandardDeviation(x) {
 
     sd = Math.sqrt(getVariance(0)) //formula for sd
     
-    if (x == 1) {
-        alert(sd)
+    if (x == 1) { // used to check if the call is coming from the button (parameter = 1) or if the call is needed for other functions 
+        alert(sd) // display value
     }
     else 
-    return sd; 
+    return sd; // return value
 }
 
 function getVariance(x) {
@@ -61,11 +61,11 @@ function getVariance(x) {
     }
 
     variance = sum/arrayNums.length //formula for variance
-    if (x == 1) {
-        alert(variance)
+    if (x == 1) { // used to check if the call is coming from the button (parameter = 1) or if the call is needed for other functions
+        alert(variance) // display value
     }
     else 
-    return variance;
+    return variance; // return value
 }
 
 
@@ -93,22 +93,22 @@ function getMean(x) {
     // get the summation of the array of numbers then divide it by how many numbers in the array
     mean = sum/arrayNums.length
 
-    if (x == 1) {
-        alert(mean)
+    if (x == 1) { // used to check if the call is coming from the button (parameter = 1) or if the call is needed for other functions
+        alert(mean) // display value
     }
     else 
-    return mean;
+    return mean; // return value
 
 }
 
 function getArrayOfNumbers() {
     var arrayOfNumbers = [];
-    arrayOfNumbers = textControl.value.split(" ");
+    arrayOfNumbers = textControl.value.split(" "); // split inputs separated by a space " " and assign to arrray
     return arrayOfNumbers;
 }
 
 function checkInputNumberOnly(str) {
     
-    return /^\d+$/.test(str);
+    return /^\d+$/.test(str); // use regex to check if input is number only 
 }
 
